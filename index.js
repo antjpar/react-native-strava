1eb8504ca0c7d5cbe1cc800271ab2ac8cef15676
valeri <valeritsert@gmail.com>
2019-03-14 20:46:37 +0400

    export RNStrava

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ import { NativeModules } from 'react-native';
 
 const { RNReactNativeStrava } = NativeModules;
 
-export default RNReactNativeStrava;
+class RNStrava {
+  static login(client_id, redirect_uri, response_type = 'code', approval_prompt = 'auto', scope = 'activity:write,read') {
+    RNReactNativeStrava.login(client_id, redirect_uri, response_type, approval_prompt, scope);
+  }
+}
+
+export default RNStrava;
