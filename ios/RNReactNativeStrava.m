eb86c8c775d6f1904ebbad528b2496843cdacd15
valeri <valeritsert@gmail.com>
2019-03-18 12:10:04 +0400

    parameters for login API

--- ios/RNReactNativeStrava.m
+++ ios/RNReactNativeStrava.m
@@ -5,9 +5,20 @@
 
 - (dispatch_queue_t)methodQueue
 {
-    return dispatch_get_main_queue();
+  return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+// client_id, redirect_uri, response_type = 'code', approval_prompt = 'auto', scope = 'activity:write,read'
+RCT_EXPORT_METHOD(login:(NSString*)client_id
+                  redirect_uri:(NSString*)redirect_uri
+                  response_type:(NSString*)response_type
+                  approval_prompt:(NSString*)approval_prompt
+                  scope:(NSString*)scope
+                  ) {
+
+}
+
+RCT_EXPORT_MODULE(RNReactNativeStrava);
 
 @end
-  
+
